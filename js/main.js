b3fdbee1f995e184b27ef4b0689598e97d73e1bd
YuliyaGrichsh <moroz_yulia99@mail.ru>
2023-06-10 23:21:37 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-function checkLength(str,maxLength) {
-    if (strlength <= maxLength) {
-        return true;
-    }
-    return false;
+function checkLength(str, maxLength) {
+  if (str, length <= maxLength) {
+    return true;
+  }
+  return false;
 }
 
 function getrandomNumber(min, max) {
-    if (min < 0) {
-        return "Ошибка!";
-    }
-    if (max <= min) {
-        return "Проверьте введённые данные!";
-    }
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) +min);
+  if (min < 0) {
+    return 'Ошибка!';
+  }
+  if (max <= min) {
+    return 'Проверьте введённые данные!';
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) +min);
 }
