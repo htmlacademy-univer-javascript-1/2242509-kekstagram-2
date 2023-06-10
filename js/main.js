5d2d1a53779da9cc42d9fa0ce2851390ad2455ce
YuliyaGrichsh <moroz_yulia99@mail.ru>
2023-06-10 17:42:07 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
-function checkStringLength (string, length) {
-    return string.length <= length;}
+function checkLength (str. maxLength) {
+    if (str.length <= maxLength) {
+        return true;
+    }
+    return false;
+}
+
+function getrandomNumber(min, max) {
+    if (min < 0) {
+        return "Ошибка!";
+    }
+    if (max <= min) {
+        return "Проверть введённые данные!";
+    }
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) +min);
+}
