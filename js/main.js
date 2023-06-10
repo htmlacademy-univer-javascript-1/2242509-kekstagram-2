5a53032bd2946ffae22f91c6e3c50c1c1b2e3598
YuliyaGrichsh <moroz_yulia99@mail.ru>
2023-06-10 17:28:48 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-// Функция взята из интернета и доработана
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
-function qetRandomPositiveInteger (a, b) {
-
-const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-
-const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-
-const result = Math.random() * (upper - lower + 1) + lower;
-
-return Math.floor(result);
-
-}
 
 function checkStringLength (string, length) {
 
