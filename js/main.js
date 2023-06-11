baaa4da92ecccb949d692dff2d3c61aebe35709c
YuliyaGrichsh <moroz_yulia99@mail.ru>
2023-06-11 18:50:25 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,21 +3,21 @@ function getRandomIntInclusive(from, to){
     throw new RangeError ('Числа в диапазоне должны бть положительными');
   }
 
-if (typeof from === 'string' || typeof to === 'string') {
- throw new RangeError ('Значения должны быть числами, а не строкой');
-}
+  if (typeof from === 'string' || typeof to === 'string') {
+    throw new RangeError ('Значения должны быть числами, а не строкой');
+  }
 
-if (from > to) {
-  [from, to] = [to, from];
-}
+  if (from > to) {
+    [from, to] = [to, from];
+  }
 
-if (from === to) {
- return to;
-}
+  if (from === to) {
+    return to;
+  }
 
-from = Math.ceil(from);
-to = Math.floor(to);
-return Math.floor(Match.random() * (to - from + 1)) + from;
+  from = Math.ceil(from);
+  to = Math.floor(to);
+  return Math.floor(Math.random() * (to - from + 1)) + from;
 }
 
 const isCorrectLength = (str, maxLength) => {
@@ -25,7 +25,7 @@ const isCorrectLength = (str, maxLength) => {
     throw new RangeError('Значени str должно быть строкой');
   }
 
-  return str.length <= maxLength; 
-}; 
+  return str.length <= maxLength;
+};
 
 export {getRandomIntInclusive, isCorrectLength};
